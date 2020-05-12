fba1b556d6974939122632f1646faad455d9112e
Konstantin <kostbg1@gmail.com>
2020-05-13 02:31:06 +0300

    deleted .so

--- srcs/ft_memcmp.c
+++ srcs/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 05:53:20 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/13 01:43:27 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/13 02:29:28 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,9 @@ int						ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char *ch_s1;
 	const unsigned char *ch_s2;
-	const unsigned long *long_s1;
-	const unsigned long *long_s2;
 
-	long_s1 = (const unsigned long*)s1;
-	long_s2 = (const unsigned long*)s2;
-	while (*long_s1 == *long_s2 && n > sizeof(long) &&
-	((*long_s1 - LOWBITS) & ~*long_s1 & HIGHBITS) == 0)
-	{
-		n = n - sizeof(long);
-		long_s1++;
-		long_s2++;
-	}
-	ch_s1 = (const unsigned char*)long_s1;
-	ch_s2 = (const unsigned char*)long_s2;
+	ch_s1 = (const unsigned char*)s1;
+	ch_s2 = (const unsigned char*)s2;
 	while (*ch_s1 == *ch_s2 && n)
 	{
 		n--;
