cde5db51f7d4ecc1073e65551b36530da5247dae
Konstantin <kostbg1@gmail.com>
2020-05-12 21:32:19 +0300

    small fixes

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 23:33:14 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/12 21:31:11 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/12 21:32:06 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char			*ft_strnstr(const char *bg, const char *ltle, size_t len)
 	const char	*ch_ltle;
 	const char	*ch_litinbg;
 
-	if (s1 == s2 || n == 0)
+	if (bg == ltle || len == 0)
 		return (0);	
 	ch_bg = bg;
 	ch_ltle = ltle;
