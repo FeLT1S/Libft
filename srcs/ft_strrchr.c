50ccc852211764d0f5835cbda8e08613ad74bf33
Konstantin <kostbg1@gmail.com>
2020-05-12 21:23:05 +0300

    small fixes

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 23:05:12 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/12 21:18:04 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/12 21:22:40 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ char					*ft_strrchr(const char *str, int c)
 	const unsigned long	*long_ch;
 	const unsigned long	long_c = (unsigned long)c * LOWBITS;
 
-	ch = str + ft_strlen(str);
-	while (((unsigned long)ch & (sizeof(long) - 1)) != 0 && ch != str)
+	ch = (unsigned char*)str + ft_strlen(str);
+	while (((unsigned long)ch & (sizeof(long) - 1)) != 0 && ch != (unsigned char*)str)
 	{
 		if (*ch == (unsigned char)c)
 			return ((void *)ch);
@@ -29,10 +29,10 @@ char					*ft_strrchr(const char *str, int c)
 	}
 	long_ch = (const unsigned long*)ch;
 	while (((((*(long_ch - 1) ^ long_c) - LOWBITS) & ~(*(long_ch - 1) ^ long_c)
-	& HIGHBITS) == 0) && (ch - str > (long int)sizeof(long)))
+	& HIGHBITS) == 0) && (ch - (unsigned char*)str > (long int)sizeof(long)))
 		--long_ch;
 	ch = (const unsigned char*)long_ch;
-	while (*ch != (char)c && str != ch)
+	while (*ch != (unsigned char)c && (unsigned char*)str != ch)
 		ch--;
 	if (*ch == (const unsigned char)c)
 		return ((void *)ch);
