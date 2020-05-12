d5ebbb3f807f6d79993c2f614b4851e90fd1d161
Konstantin <kostbg1@gmail.com>
2020-05-12 21:34:01 +0300

    small fixes

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 00:13:32 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/12 21:11:20 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/12 21:33:51 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,7 @@ void					*ft_memmove(void *dst, const void *src, size_t n)
 		return (ft_memcpy(dst, src, n));
 	long_dst = (unsigned long*)(dst + n);
 	long_src = (const unsigned long*)(src + n);
-	while (n > sizeof(long) &&
-	((*long_src - LOWBITS) & ~*long_src & HIGHBITS) == 0)
+	while (n > sizeof(long))
 	{
 		*(--long_dst) = *(--long_src);
 		n = n - sizeof(long);
