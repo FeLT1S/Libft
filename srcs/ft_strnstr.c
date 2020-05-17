40873b9b9b1d6f26f3811771cd6c28781027c938
Konstantin <kostbg1@gmail.com>
2020-05-17 17:43:55 +0300

    strnstr fix

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 23:33:14 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/14 01:10:27 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/17 17:43:14 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,15 @@
 char			*ft_strnstr(const char *bg, const char *ltle, size_t len)
 {
 	const char	*ch_litinbg;
+	const size_t ltle_len = ft_strlen(ltle);
 
 	if (*ltle == '\0')
-		return ((void*)bg);
-	while (*bg != *ltle && len && *bg != '\0')
+		return ((char*)bg);
+	while (ltle_len <= len-- && *bg != '\0')
 	{
+		if (*bg == *ltle && ft_strncmp(bg, ltle, ltle_len) == 0)
+			return ((char *)bg);
 		bg++;
-		len--;
 	}
-	ch_litinbg = bg;
-	while (*ltle == *bg && len && *ltle != '\0')
-	{
-		ltle++;
-		bg++;
-		len--;
-	}
-	if (*ltle == '\0')
-		return ((char*)ch_litinbg);
 	return (0);
 }
