7740eda16b5c8c0fc7b0b65eea8323461f18f9b2
Konstantin <kostbg1@gmail.com>
2020-05-18 20:52:09 +0300

    memmove fix

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,9 +6,13 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 00:13:32 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/18 20:32:47 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/18 20:51:21 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+#define LOWBITS ((unsigned long)-1 / (unsigned char)-1)
+#define HIGHBITS (LOWBITS << 7)
 
 #include "libft.h"
 #define LOWBITS ((unsigned long)-1 / (unsigned char)-1)
@@ -36,15 +40,13 @@ static void				*ft_memrcpy(void *dst, const void *src, size_t n)
 	}
 	mod_dst = (unsigned char*)long_dst - 1;
 	mod_src = (const unsigned char*)long_src - 1;
-	while (n-- + 1)
+	while (n--)
 		*mod_dst-- = *mod_src--;
 	return (dst);
 }
 
 void					*ft_memmove(void *dst, const void *src, size_t n)
 {
-	if (!n)
-		return (dst);
 	if (dst < src)
 		return (ft_memcpy(dst, src, n));
 	if (dst > src)
