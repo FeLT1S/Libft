320a6238200e9c9f89d480781ef96cf2da7fe280
Konstantin <kostbg1@gmail.com>
2020-05-18 17:24:41 +0300

    Karamaz fix

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 04:47:34 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/18 17:12:41 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/18 17:24:38 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *dst;
 
-	if (!s || !(len + 1))
-		return (0);
-	if (start > ft_strlen(s))
-		return (NULL);
+	if (!s || len <= 0 || start >= ft_strlen(s))
+		return (ft_strdup(""));
 	dst = (char *)malloc(sizeof(char) * (len + 1));
 	if (!dst)
 		return (0);
