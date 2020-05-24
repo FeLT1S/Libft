911ac9bbbbee7d4c39a504a9f8dc44ad9f60f30f
Konstantin <kostbg1@gmail.com>
2020-05-24 20:10:23 +0300

    fixed memchr

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 04:22:41 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/24 19:30:43 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/24 20:08:55 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 #define LOWBITS ((unsigned long)-1 / (unsigned char)-1)
 #define HIGHBITS (LOWBITS << 7)
 
+static unsigned long	*long_memchr(size_t long_c, size_t *long_ch, size_t n)
+{
+	while (((((*long_ch ^ long_c) - LOWBITS) & ~(*long_ch ^ long_c) \
+	& HIGHBITS) == 0) && (n > sizeof(long)))
+	{
+		n = n - sizeof(long);
+		long_ch++;
+	}
+	return(long_ch);
+}
+
 void					*ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char	*ch = s;
 	const unsigned long	*long_ch;
 	const unsigned long	long_c = (unsigned char)c * LOWBITS;
 
+	if (!n)
+		return (NULL);
 	while (((unsigned long)ch & (sizeof(long) - 1)) != 0 && n)
 	{
 		if (*ch == (unsigned char)c)
@@ -28,17 +41,11 @@ void					*ft_memchr(const void *s, int c, size_t n)
 		n--;
 	}
 	long_ch = (unsigned long*)ch;
-	while (((((*long_ch ^ long_c) - LOWBITS) & ~(*long_ch ^ long_c) \
-	& HIGHBITS) == 0) && (n > sizeof(long)))
-	{
-		n = n - sizeof(long);
-		long_ch++;
-	}
-	ch = (unsigned char*)long_ch;
+	ch = (unsigned char*)long_memchr(long_c, long_ch, n);
 	while (*ch != (unsigned char)c && n)
 	{
 		ch++;
 		n--;
 	}
-	return ((void *)(n == (size_t)-1 ? NULL : ch));
+	return ((void *)(!n ? NULL : ch));
 }
