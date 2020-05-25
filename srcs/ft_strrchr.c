ea23f91e4e6c902a87a250807bbe2c89bb5d5da7
Konstantin <kostbg1@gmail.com>
2020-05-25 06:08:26 +0300

    newstrrchr

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 23:05:12 by jiandre           #+#    #+#             */
-/*   Updated: 2020/05/25 06:00:24 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/05/25 06:06:55 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 char					*ft_strrchr(const char *str, int c)
 {
-	const char *sym = ft_strchr(str, c);
+	const char *sym;
 	const char *tmp = NULL;
 
 	if (!c)
-		return (str + ft_strlen(str));
+		return ((char *)str + ft_strlen(str));
+	sym = ft_strchr(str, c);
 	while(sym)
 	{
 		tmp = sym;
