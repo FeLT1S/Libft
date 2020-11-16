1d4ae35fcb55cd2494711d9ee0718ee47a0b9c17
felt1s <kostbg1@gmail.com>
2020-11-16 16:37:12 +0300

    fixed delete dlst

--- inc/dlst.h
+++ inc/dlst.h
@@ -6,7 +6,7 @@
 /*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 16:19:11 by jiandre           #+#    #+#             */
-/*   Updated: 2020/11/02 20:36:02 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/11/16 16:26:31 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void				dlst_push_front(t_node **root, t_node *new_elem);
 t_node				*dlst_pop_back(t_node **root);
 t_node				*dlst_pop_front(t_node **root);
 t_node				*dlst_elem(void *content);
-void				dlst_del_elem(t_node *node, t_del_func deletor);
+void				dlst_del_elem(t_node **root, t_node *node, t_del_func deletor);
 void				dlst_del(t_node **root, t_del_func deletor);
 size_t				dlst_size(t_node **root);
 
--- srcs/dlst_del_elem.c
+++ srcs/dlst_del_elem.c
@@ -3,23 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   dlst_del_elem.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jiandre <kostbg1@gmail.com>                +#+  +:+       +#+        */
+/*   By: jiandre <jiandre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 16:07:00 by jiandre           #+#    #+#             */
-/*   Updated: 2020/11/14 16:42:52 by jiandre          ###   ########.fr       */
+/*   Updated: 2020/11/16 16:30:17 by jiandre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void				dlst_del_elem(t_node *node, t_del_func deletor)
+void				dlst_del_elem(t_node **root, t_node *node, t_del_func deletor)
 {
-	if (node->prev)
-		node->prev->next = node->next;
-	if (node->next)
-		node->next->prev = node->prev;
-	deletor(node->content);
-	node->prev = NULL;
-	node->next = NULL;
-	free(node);
+	t_node *tmp;
+	
+	if (node == *root)
+	{
+		tmp = (*root)->next;
+		if (node->next)
+			node->next->prev = NULL;
+		deletor(node->content);
+		node->next = NULL;
+		free(node);
+		*root = tmp;
+	}
+	else
+	{
+		if (node->prev)
+			node->prev->next = node->next;
+		if (node->next)
+			node->next->prev = node->prev;
+		deletor(node->content);
+		node->prev = NULL;
+		node->next = NULL;
+		free(node);
+	}
 }
